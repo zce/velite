97edd037251f9e99631a0e53b221cd22142df766
zce <w@zce.me>
2023-11-12 19:55:41 +0800

    feat: update shared shema

--- src/shared.ts
+++ src/shared.ts
@@ -8,13 +8,13 @@ export const shared = {
   title: z.string().max(99),
   slug: z.string().min(3).regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, 'Invalid slug'),
   date: z.string().transform(value => new Date(value).toISOString()),
-  paragraph: z.string().max(999).optional(),
+  paragraph: z.string().max(999),
   meta: z.object({ title: z.string().optional(), description: z.string().optional(), keywords: z.array(z.string()).optional() }).default({}),
-  file: z.string().optional().transform((value, ctx) => outputFile(value, ctx.path[0] as string).catch(err => {
+  file: z.string().transform((value, ctx) => outputFile(value, ctx.path[0] as string).catch(err => {
     ctx.addIssue({ code: 'custom', message: err.message })
     return value
   })),
-  image: z.string().optional().transform((value, ctx) => outputImage(value, ctx.path[0] as string).catch(err => {
+  image: z.string().transform((value, ctx) => outputImage(value, ctx.path[0] as string).catch(err => {
     ctx.addIssue({ code: 'custom', message: err.message })
     return value
   })),

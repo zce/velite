de9def9aaa6be45ebcb63fca3b6864e104206329
zce <w@zce.me>
2023-11-26 10:25:52 +0800

    chore: rename shared to shemas

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from './shared'
+export * from './schemas'
 export { defineConfig, defineCollection, defineLoader, type UserConfig, type Collection, type Loader, type Image } from './types'
 export { addLoader, removeLoader } from './loaders'
 export { build, type BuildOptions } from './build'

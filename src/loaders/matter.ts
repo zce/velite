7581ebb284f61d190abaf1051fd32bf3752e9ac5
zce <w@zce.me>
2023-12-06 17:10:27 +0800

    chore: meta tweaks

--- src/loaders/matter.ts
+++ src/loaders/matter.ts
@@ -3,14 +3,14 @@ import yaml from 'yaml'
 import { defineLoader } from '../config'
 
 // https://github.com/vfile/vfile-matter/blob/main/lib/index.js
-const matterRegex = /^---(?:\r?\n|\r)(?:([\s\S]*?)(?:\r?\n|\r))?---(?:\r?\n|\r|$)/
+const MATTER_RE = /^---(?:\r?\n|\r)(?:([\s\S]*?)(?:\r?\n|\r))?---(?:\r?\n|\r|$)/
 
 export default defineLoader({
   // name: 'matter',
   test: /\.(md|mdx)$/,
   load: async file => {
     const raw = file.toString().trim()
-    const match = raw.match(matterRegex)
+    const match = raw.match(MATTER_RE)
     return {
       data: match == null ? {} : yaml.parse(match[1]),
       content: match == null ? raw : raw.slice(match[0].length).trim()

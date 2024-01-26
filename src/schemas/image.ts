129eff13a44ba90c4eaf2e71fc2437dc3f4347a8
zce <w@zce.me>
2024-01-26 14:13:00 +0800

    chore: meta tweaks

--- src/schemas/image.ts
+++ src/schemas/image.ts
@@ -16,6 +16,7 @@ export interface ImageOptions {
  */
 export const image = ({ allowNonRelativePath = false }: ImageOptions = {}) =>
   string().transform<Image>((value, { meta: { file, config }, addIssue }) => {
+    // TODO: support absolute path metadata
     if (allowNonRelativePath && !isRelativePath(value)) return { src: value, width: 0, height: 0, blurDataURL: '', blurWidth: 0, blurHeight: 0 }
     return processAsset(value, file.path, config.output.name, config.output.base, true).catch(err => {
       addIssue({ code: 'custom', message: err.message })

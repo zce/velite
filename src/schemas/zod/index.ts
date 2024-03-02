64b4bc4bf9c283dd5c4eded297b8b6263b02d1da
zce <w@zce.me>
2024-03-02 17:34:25 +0800

    chore: meta tweaks

--- src/schemas/zod/index.ts
+++ src/schemas/zod/index.ts
@@ -1,4 +1,4 @@
 // don't edit this directory manually
 // this is a fork of zod
-// https://github.com/zce/zod/tree/41c82f83415c0d659beb7000953fa91e18e8f237
+// https://github.com/colinhacks/zod/pull/3023
 export * from './external'

0a362f986318fe42ddca19607994b48c9921ac99
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2024-03-03 17:26:59 +0100

    feat: add option to remove index from flattened path (#60)
    * fix: leave index to flattened path
    
    * feat: add option for removing index
    
    * docs: update default

--- src/schemas/path.ts
+++ src/schemas/path.ts
@@ -3,18 +3,35 @@ import { relative } from 'node:path'
 import { custom } from './zod'
 
 /**
+ * Options for flattened path
+ * extraction
+ */
+export interface PathOptions {
+  /**
+   * removes `index` from the path
+   * for subfolders
+   *
+   * @default true
+   */
+  removeIndex?: boolean
+}
+
+/**
  * Flattened path
+ * @param options - options for the path flattening
+ *
  * @returns flattened path based on the file path
  */
-export const path = () =>
+export const path = (options?: PathOptions) =>
   custom<string>().transform<string>(async (value, { meta: { path, config }, addIssue }) => {
     if (value != null) {
       addIssue({ code: 'custom', message: '`s.path()` schema will resolve the flattening path based on the file path' })
       return null as never
     }
 
-    return relative(config.root, path)
+    const flattened = relative(config.root, path)
       .replace(/\.[^.]+$/, '')
       .replace(/\\/g, '/')
-      .replace(/\/index$/, '')
+
+    return options?.removeIndex === false ? flattened : flattened.replace(/\/index$/, '')
   })

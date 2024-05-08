17bbb6f5f35f8d224f24cd52bde8f960ce73bc63
zce <w@zce.me>
2024-05-08 12:33:29 +0800

    fix: `s.path()` schema warning

--- src/schemas/path.ts
+++ src/schemas/path.ts
@@ -26,7 +26,6 @@ export const path = (options?: PathOptions) =>
   custom<string>().transform<string>(async (value, { meta: { path, config }, addIssue }) => {
     if (value != null) {
       addIssue({ code: 'custom', message: '`s.path()` schema will resolve the flattening path based on the file path' })
-      return null as never
     }
 
     const flattened = relative(config.root, path)

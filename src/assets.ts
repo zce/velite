bd4d9afba00b0f85a551046b9b8fd72ccc7d40e2
Danylo <44447172+Kotyarishka@users.noreply.github.com>
2024-12-19 11:02:00 +0000

    fix: assign minimal value for blurHeight

--- src/assets.ts
+++ src/assets.ts
@@ -70,7 +70,7 @@ export const getImageMetadata = async (buffer: Buffer): Promise<Omit<Image, 'src
   if (width == null || height == null) return
   const aspectRatio = width / height
   const blurWidth = 8
-  const blurHeight = Math.round(blurWidth / aspectRatio)
+  const blurHeight = Math.min(1, Math.round(blurWidth / aspectRatio))
   const blurImage = await img.resize(blurWidth, blurHeight).webp({ quality: 1 }).toBuffer()
   const blurDataURL = `data:image/webp;base64,${blurImage.toString('base64')}`
   return { height, width, blurDataURL, blurWidth, blurHeight }

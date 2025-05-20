7835f41dd05e78c041e50339fca2da27649bca6e
Lei <w@zce.me>
2025-05-19 20:21:05 -0700

    chore: use tsup insead of rollup (#320)

--- examples/nextjs/next.config.ts
+++ examples/nextjs/next.config.ts
@@ -1,12 +1,10 @@
-import { build } from 'velite'
-
 import type { NextConfig } from 'next'
 
 const isDev = process.argv.indexOf('dev') !== -1
 const isBuild = process.argv.indexOf('build') !== -1
 if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
   process.env.VELITE_STARTED = '1'
-  build({ watch: isDev, clean: !isDev })
+  import('velite').then(m => m.build({ watch: isDev, clean: !isDev }))
 }
 
 const nextConfig: NextConfig = {

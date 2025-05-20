187864da3b63ac1635bb4db3abe8316bed9b71c2
zce <w@zce.me>
2025-05-19 18:31:50 -0700

    chore: update nextjs example

--- examples/nextjs/next.config.ts
+++ examples/nextjs/next.config.ts
@@ -1,10 +1,12 @@
+import { build } from 'velite'
+
 import type { NextConfig } from 'next'
 
 const isDev = process.argv.indexOf('dev') !== -1
 const isBuild = process.argv.indexOf('build') !== -1
 if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
   process.env.VELITE_STARTED = '1'
-  import('velite').then(m => m.build({ watch: isDev, clean: !isDev }))
+  build({ watch: isDev, clean: !isDev })
 }
 
 const nextConfig: NextConfig = {
